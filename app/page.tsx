f927d17795d1f49e79c4e3149cbe4271edfbe481
aa5ba <fwm7yjjrdr@privaterelay.appleid.com>
2026-05-05 12:54:14 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -57,8 +57,12 @@ export default function Home() {
 
   const converted = value
     .toString()
-   .replace(/[٠-٩]/g, (d: string) => String(d.charCodeAt(0) - 1632))
-.replace(/[۰-۹]/g, (d: string) => String(d.charCodeAt(0) - 1776))
+.replace(/\u0660|\u0661|\u0662|\u0663|\u0664|\u0665|\u0666|\u0667|\u0668|\u0669/g, (d: string) =>
+  String(d.charCodeAt(0) - 1632)
+)
+.replace(/\u06F0|\u06F1|\u06F2|\u06F3|\u06F4|\u06F5|\u06F6|\u06F7|\u06F8|\u06F9/g, (d: string) =>
+  String(d.charCodeAt(0) - 1776)
+)
     .replace(/٫/g, ".")
     .replace(/,/g, "")
     .replace(/[^0-9.]/g, "")
