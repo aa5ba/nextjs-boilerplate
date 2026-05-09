4dd771b6b2b29abc1b09c9a5b545bf9c762ef249
aa5ba <fwm7yjjrdr@privaterelay.appleid.com>
2026-05-10 01:06:35 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl = "https://rqgzoeyyojfweyoewhvev.supabase.co"
+const supabaseUrl = "https://rqgzoeyyojfwyoewvhev.supabase.co"
 
 const supabaseAnonKey =
   "sb_publishable_Zt56a_KLr3rtcdqI7slvCg_mSrB0ZoM"
