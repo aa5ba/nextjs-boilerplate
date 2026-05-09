a70336361fd3ae449f45e0389ef26c35ca69d7ed
aa5ba <fwm7yjjrdr@privaterelay.appleid.com>
2026-05-10 00:58:00 +0300

    Create Supabase client

---
+++ lib/supabaseClient.ts
@@ -0,0 +1,11 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = "https://rqgzoeyyojfweyoewhvev.supabase.co"
+
+const supabaseAnonKey =
+  "sb_publishable_Zt56a_KLr3rtcdqI7slvCg_mSrB0ZoM"
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseAnonKey
+)
