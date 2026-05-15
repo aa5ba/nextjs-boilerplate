36385c99c1bd3e019b03d954d5c988ec6705e26e
aa5ba <fwm7yjjrdr@privaterelay.appleid.com>
2026-05-15 20:15:32 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js"
 const supabaseUrl = "https://rqgzoeyyojfwyoewvhev.supabase.co"
 
 const supabaseAnonKey =
-  "sb_publishable_Zt56a_KLr3rtcdqI7slvCg_mSrB0ZoM"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxZ3pvZXl5b2pmd3lvZXd2aGV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzMTYwMzksImV4cCI6MjA5Mzg5MjAzOX0.HQQpBkihphcB1BvNBeMr5Btr-Fvh3r5gid-GmuZIvnk"
 
 export const supabase = createClient(
   supabaseUrl,
