477c7b48eea0f660ad7cff2aecb3dd0849dad789
aa5ba <fwm7yjjrdr@privaterelay.appleid.com>
2026-06-26 08:25:20 +0300

    Update page.tsx

--- app/finance/[branch]/contracts/promissory-note/new/page.tsx
+++ app/finance/[branch]/contracts/promissory-note/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+ 
 import { useEffect, useMemo, useRef, useState } from "react";
 import type { CSSProperties, ReactNode } from "react";
 import { useParams, useRouter } from "next/navigation";
